426dd918648bf2390ed65fb91c2c0278ea60b854
Vasu Sharma <vasusharma2017@outlook.comwq>
2020-12-21 16:25:12 +0530

    adding boiler plate code

---
+++ src/main/java/com/winternewtech/sendmailembedder/SendMailEmbedderApplication.java
@@ -0,0 +1,13 @@
+package com.winternewtech.sendmailembedder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SendMailEmbedderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SendMailEmbedderApplication.class, args);
+	}
+
+}
