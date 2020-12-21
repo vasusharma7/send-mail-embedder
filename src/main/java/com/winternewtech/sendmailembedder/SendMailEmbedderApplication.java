a004048d017c81cfaf5c3ffaff4ba55a372e295f
Vasu Sharma <vasusharma2017@outlook.comwq>
2020-12-21 18:55:12 +0530

    added basic post

--- src/main/java/com/winternewtech/sendmailembedder/SendMailEmbedderApplication.java
+++ src/main/java/com/winternewtech/sendmailembedder/SendMailEmbedderApplication.java
@@ -2,12 +2,22 @@ package com.winternewtech.sendmailembedder;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @SpringBootApplication
+@RestController
 public class SendMailEmbedderApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SendMailEmbedderApplication.class, args);
+	}
+
+	@PostMapping("/register")
+	public String register() {
+		return "You are registered";
 	}
 
 }
