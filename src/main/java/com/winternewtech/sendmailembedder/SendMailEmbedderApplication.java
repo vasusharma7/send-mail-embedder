1a4f98dc4da806c7b719ece40f2ead83970d8cd7
Vasu Sharma <vasusharma2017@outlook.comwq>
2020-12-21 20:44:02 +0530

    parameters received at backend

--- src/main/java/com/winternewtech/sendmailembedder/SendMailEmbedderApplication.java
+++ src/main/java/com/winternewtech/sendmailembedder/SendMailEmbedderApplication.java
@@ -1,5 +1,7 @@
 package com.winternewtech.sendmailembedder;
 
+import com.winternewtech.sendmailembedder.*;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,9 +17,10 @@ public class SendMailEmbedderApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SendMailEmbedderApplication.class, args);
 	}
-	
-	@PostMapping("/register")
-	public String register(@RequestBody String user) {
-		return user;
+
+	@PostMapping(value = "/register", consumes = "application/json", produces = "application/json")
+	public String register(@RequestBody RegisterData data) {
+		System.out.println(String.format("%s %s %s", data.name, data.email, data.project));
+		return "hey";
 	}
 }
---
+++ src/main/java/com/winternewtech/sendmailembedder/utils.java
@@ -0,0 +1,16 @@
+package com.winternewtech.sendmailembedder;
+
+class RegisterData {
+    public String name;
+    public String email;
+    public String project;
+
+}
+
+class RegisterService {
+
+    public static void saveRegisterData(RegisterData data) {
+        //
+    }
+
+}
